9918eec1b4f6028262c82b1b36cf148471a8703c
Sal Tamay <sal.tamay@outlook.com>
2020-01-26 17:41:19 -0500

    feat: Change navigation options color when selected

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,13 +1,37 @@
 import React from 'react';
 
 class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: 'All'
+    };
+  }
+
+  updateLanguage = selectedLanguage => {
+    this.setState({
+      selectedLanguage
+    });
+  };
+
   render() {
     const languages = ['All', 'JavaScript', 'Ruby', 'CSS', 'Python'];
     return (
       <ul className='flex-center'>
         {languages.map(language => (
           <li key={language}>
-            <button className='btn-clear nav-link'>{language}</button>
+            <button
+              className='btn-clear nav-link'
+              style={
+                language === this.state.selectedLanguage
+                  ? { color: 'rgb(187, 46, 31)' }
+                  : null
+              }
+              onClick={() => this.updateLanguage(language)}
+            >
+              {language}
+            </button>
           </li>
         ))}
       </ul>
