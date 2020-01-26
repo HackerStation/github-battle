ca81ea76f72c852b899af30f83f9adddea788b71
Sal Tamay <sal.tamay@outlook.com>
2020-01-26 18:10:33 -0500

    feat: Separate navbar as a functional component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,24 @@
 import React from 'react';
 
+const LanguagesNav = ({ selected, onUpdateLanguage }) => {
+  const languages = ['All', 'JavaScript', 'Ruby', 'CSS', 'Python'];
+  return (
+    <ul className='flex-center'>
+      {languages.map(language => (
+        <li key={language}>
+          <button
+            className='btn-clear nav-link'
+            style={language === selected ? { color: 'rgb(187, 46, 31)' } : null}
+            onClick={() => onUpdateLanguage(language)}
+          >
+            {language}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
 class Popular extends React.Component {
   constructor(props) {
     super(props);
@@ -16,25 +35,11 @@ class Popular extends React.Component {
   };
 
   render() {
-    const languages = ['All', 'JavaScript', 'Ruby', 'CSS', 'Python'];
     return (
-      <ul className='flex-center'>
-        {languages.map(language => (
-          <li key={language}>
-            <button
-              className='btn-clear nav-link'
-              style={
-                language === this.state.selectedLanguage
-                  ? { color: 'rgb(187, 46, 31)' }
-                  : null
-              }
-              onClick={() => this.updateLanguage(language)}
-            >
-              {language}
-            </button>
-          </li>
-        ))}
-      </ul>
+      <LanguagesNav
+        selected={this.state.selectedLanguage}
+        onUpdateLanguage={this.updateLanguage}
+      />
     );
   }
 }
