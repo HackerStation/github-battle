471efd98dbc3f5f976ae3c63b4d7c02e6645f952
Sal Tamay <sal.tamay@outlook.com>
2020-01-26 21:33:25 -0500

    feat: display repos

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { fetchPopularRepos } from '../utils/api';
+import {
+  FaUser,
+  FaStar,
+  FaCodeBranch,
+  FaExclamationTriangle
+} from 'react-icons/fa';
 
 const LanguagesNav = ({ selected, onUpdateLanguage }) => {
   const languages = ['All', 'JavaScript', 'Ruby', 'CSS', 'Python'];
@@ -24,6 +30,57 @@ const LanguagesNav = ({ selected, onUpdateLanguage }) => {
 LanguagesNav.propTypes = {
   selected: PropTypes.string.isRequired,
   onUpdateLanguage: PropTypes.func.isRequired
+};
+
+const ReposGrid = ({ repos }) => {
+  return (
+    <ul className='grid space-around'>
+      {repos.map((repo, index) => {
+        const {
+          name,
+          owner,
+          html_url,
+          stargazers_count,
+          forks,
+          open_issues
+        } = repo;
+        const { login, avatar_url } = owner;
+        return (
+          <li key={html_url} className='repo bg-light'>
+            <h4 className='header-lg center-text'>#{index + 1}</h4>
+            <img
+              src={avatar_url}
+              className='avatar'
+              alt={`Avatar for ${login}`}
+            />
+            <h2 className='center-text'>
+              <a href={html_url} className='link'>
+                {name}
+              </a>
+            </h2>
+            <ul className='card-list'>
+              <li>
+                <FaUser color='rgb(255, 191, 116)' size={22} />
+                <a href={`https://www.github.com/${login}`}>{login}</a>
+              </li>
+              <li>
+                <FaStar color='rgb(255, 215, 0)' size={22} />
+                {stargazers_count.toLocaleString()} stars
+              </li>
+              <li>
+                <FaCodeBranch color='rgb(129, 195, 245)' size={22} />
+                {forks.toLocaleString()} forks
+              </li>
+              <li>
+                <FaExclamationTriangle color='rgb(239, 174, 180)' size={22} />
+                {open_issues.toLocaleString()} open
+              </li>
+            </ul>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 class Popular extends React.Component {
@@ -82,7 +139,9 @@ class Popular extends React.Component {
         />
         {this.isLoading() && <p>LOADING...</p>}
         {error && <p>{error}</p>}
-        {repos && <pre>{JSON.stringify(repos[selectedLanguage], null, 2)}</pre>}
+        {repos[selectedLanguage] && (
+          <ReposGrid repos={repos[selectedLanguage]} />
+        )}
       </React.Fragment>
     );
   }
