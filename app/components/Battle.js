983a1df6d47851c489b8d81d700cfaf298b10072
Sal Tamay <sal.tamay@outlook.com>
2020-01-28 14:07:48 -0500

    feat: Add PlayerInput component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaUserFriends, FaFighterJet, FaTrophy } from 'react-icons/fa';
+import PropTypes from 'prop-types';
 
 const Instructions = () => {
   return (
@@ -27,7 +28,74 @@ const Instructions = () => {
   );
 };
 
+class PlayerInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: ''
+    };
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.props.onSubmit(this.state.username);
+  };
+
+  handleChange = e => {
+    this.setState({
+      username: e.target.value
+    });
+  };
+
+  render() {
+    return (
+      <form className='column player' onSubmit={this.handleSubmit}>
+        <label htmlFor='username' className='player-label'>
+          {this.props.label}
+        </label>
+        <div className='row player-inputs'>
+          <input
+            type='text'
+            name='username'
+            className='input-light'
+            placeholder='GitHub Username'
+            autoComplete='off'
+            onChange={this.handleChange}
+            value={this.state.username}
+          />
+          <button
+            className='btn dark-btn'
+            type='submit'
+            disabled={!this.state.username}
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
+
+PlayerInput.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired
+};
+
 class Battle extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userDetails: null
+    };
+  }
+
+  getUserDetails = username => {
+    console.log(username);
+  };
+
   render() {
     return (
       <React.Fragment>
