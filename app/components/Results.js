6f5d5be195a1a8b823d4a516e0db6f0b1f381c88
Sal Tamay <sal.tamay@outlook.com>
2020-01-29 21:17:42 -0500

    feat: Update Results state with the results data

--- app/components/Results.js
+++ app/components/Results.js
@@ -2,19 +2,42 @@ import React from 'react';
 import { battle } from '../utils/api';
 
 class Results extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      winner: null,
+      loser: null,
+      error: null,
+      isLoading: true
+    };
+  }
+
   componentDidMount() {
     const { playerOne, playerTwo } = this.props;
 
-    battle([playerOne, playerTwo]).then(results => {
-      console.log(results);
-    });
+    battle([playerOne, playerTwo])
+      .then(results => {
+        this.setState({
+          winner: results[0],
+          loser: results[1],
+          isLoading: false,
+          error: null
+        });
+      })
+      .catch(({ message }) => {
+        this.setState({
+          error: message,
+          isLoading: false
+        });
+      });
   }
 
   render() {
     return (
       <div>
         RESULTS...
-        <pre>{JSON.stringify(this.props, null, 2)}</pre>
+        <pre>{JSON.stringify(this.state, null, 2)}</pre>
       </div>
     );
   }
