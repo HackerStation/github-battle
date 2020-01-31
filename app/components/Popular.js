4526a847ee721fe32104a8239498a16397bda185
Sal Tamay <sal.tamay@outlook.com>
2020-01-31 00:25:53 -0500

    feat: Add Tooltip to Popular component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { fetchPopularRepos } from '../utils/api';
 import Card from './Card';
 import Loading from './Loading';
+import Tooltip from './Tooltip';
 import {
   FaUser,
   FaStar,
@@ -57,8 +58,10 @@ const ReposGrid = ({ repos }) => {
             >
               <ul className='card-list'>
                 <li>
-                  <FaUser color='rgb(255, 191, 116)' size={22} />
-                  <a href={`https://www.github.com/${login}`}>{login}</a>
+                  <Tooltip text='GitHub Username'>
+                    <FaUser color='rgb(255, 191, 116)' size={22} />
+                    <a href={`https://www.github.com/${login}`}>{login}</a>
+                  </Tooltip>
                 </li>
                 <li>
                   <FaStar color='rgb(255, 215, 0)' size={22} />
